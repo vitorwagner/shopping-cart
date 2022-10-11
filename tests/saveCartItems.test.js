85dd4a191c7d3be83a8cd29974af9599fe60d6b0
Vitor Wagner <vitormwagner@gmail.com>
2022-10-11 17:09:46 -0300

    TDD Requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,15 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('1 - Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', () => {
+    saveCartItems('cartItem');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  test('2 - Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros', () => {
+    saveCartItems('cartItem');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', 'cartItem');
+  });
+
+  // fail('Teste vazio');
 });
