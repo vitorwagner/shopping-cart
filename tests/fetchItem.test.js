26bc1d97d19cb20647a8855d8c3cc198ab505443
Vitor Wagner <vitormwagner@gmail.com>
2022-10-11 15:33:45 -0300

    TDD Requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,27 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('1 - Teste se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+
+  test('2 - Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  test('3 - Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint correto', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+
+  test('4 - Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto importado', async () => {
+    await expect(fetchItem('MLB1615760527')).resolves.toEqual(item);
+  });
+
+  test('5 - Teste se, ao chamar a função fetchItem sem argumento, retorna um erro', async () => {
+    await expect(fetchItem()).rejects.toThrow(new Error('You must provide an url'));
+  });
+
+  // fail('Teste vazio');
 });
